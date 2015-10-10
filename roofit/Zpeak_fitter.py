d2022f73c18f3caccc7a1a00d11007ed867c6f9c
gfasanel <gfasanel@cern.ch>
2015-10-10 17:55:18 +0200

    adding log scale plots

--- roofit/Zpeak_fitter.py
+++ roofit/Zpeak_fitter.py
@@ -1,9 +1,11 @@
-#python Zpeak_fitter.py -t resolution
+#python Zpeak_fitter.py
 import ROOT
 import math
 import os
 ROOT.gSystem.Load("libRooFit")
 ROOT.gSystem.Load("RooDCBShape_cxx.so") #This is the way to handle user defined pdf (generated with RooClassFactory). Compile once and for all in ROOT and then add it as a library
+ROOT.gSystem.Load("../rootlogon_style_C.so")
+ROOT.rootlogon_style()
 from ROOT import RooFit, RooRealVar, RooGaussian, RooDataSet, RooArgList, RooTreeData, RooArgSet
 ROOT.gROOT.SetBatch(ROOT.kTRUE) 
 
@@ -13,64 +15,22 @@ print "################################### FITTING THE Z PEAK ##################
 for var_type in ['data','MC']:
    for regions in ['BB','BE','EE']:
       if var_type=='MC':
-         file_mass=ROOT.TFile('../Extra_sigma/MC_Zpeak.root','READ')
-         #file_mass=ROOT.TFile('../Extra_sigma/MC_Zpeak_test.root','READ')
+         file_mass=ROOT.TFile('/user/gfasanel/Mass_resolution_study/Extra_sigma/MC_Zpeak.root','READ')
       else:
-         file_mass=ROOT.TFile('../Extra_sigma/data_Zpeak.root','READ')
-         #file_mass=ROOT.TFile('../Extra_sigma/data_Zpeak_test.root','READ')
+         file_mass=ROOT.TFile('/user/gfasanel/Mass_resolution_study/Extra_sigma/data_Zpeak.root','READ')
       hist_res   = file_mass.Get(str('h_mee_'+var_type+'_'+regions))
       hist_res.Rebin(3) #1.5 GeV binning
       max=hist_res.GetXaxis().GetBinCenter(hist_res.GetMaximumBin())
       mean_histo=hist_res.GetMean()
       x=ROOT.RooRealVar("x","m_{ee} [GeV]",60,120)
-      #x=ROOT.RooRealVar("x","m_{ee} [GeV]",80,100)
+      dh=ROOT.RooDataHist("dh","dh",RooArgList(x), hist_res)
 
-      # Create a binned dataset (RooDataHist) that imports contents of TH1 and associates its contents to observable 'x'
-      dh=ROOT.RooDataHist("dh","dh",RooArgList(x), hist_res)  #Without RooArgList it doesn't work
-
-      # P l o t   a n d   f i t   a   R o o D a t a H i s t
-      # ---------------------------------------------------
-   
       # Make plot of binned dataset showing Poisson error bars (RooFit default)
       frame = x.frame(RooFit.Name(""),RooFit.Title(" ")) #Title(" ") takes away "Rooplot of x"
       #frame is a RooPlot generated from the RooRealVar x
       #to change x range: RooFit.Range(xmin,xmax)
       frame.SetMaximum(2*hist_res.GetMaximum())
-   
       dh.plotOn(frame)  
-   
-      #mean_guessed=hist_res.GetXaxis().GetBinCenter(hist_res.GetMaximumBin())
-      #sigma_guessed=hist_res.GetRMS()/4.
-      ##sigma_guessed=hist_res.GetRMS() #only Breit Wigner
-      #print "SIGMA GUESSED ",sigma_guessed
-      #mean=ROOT.RooRealVar("mean","mean",mean_guessed,mean_guessed -0.2*sigma_guessed, mean_guessed + 0.2*sigma_guessed)
-      #sigma=ROOT.RooRealVar("sigma","sigma",sigma_guessed,0.,4) 
-      #width=ROOT.RooRealVar("width","width",sigma_guessed,0.,4) 
-      #alpha=ROOT.RooRealVar("alpha","alpha",0.1,0.,1.5) #after alpha*sigma, gaussian connected to power law: alpha>0 => left tail alpha<0 => right tail
-      #alphaR=ROOT.RooRealVar("alphaR","alphaR",2,0.1,15)#2 was already ~good
-      #if regions in ['BB']:
-      #   mean=ROOT.RooRealVar("mean","mean",mean_histo,mean_histo -0.2*sigma_guessed, mean_histo + 0.2*sigma_guessed)
-      #   n=ROOT.RooRealVar("nL","nL",2,0.1,15) #exponent of the power law tail
-      #   nR=ROOT.RooRealVar("nR","nR",3,0.1,10)
-      #elif var_type in ['MC'] and regions in ['EE']:
-      #   mean=ROOT.RooRealVar("mean","mean",mean_histo,mean_histo -0.2*sigma_guessed, mean_histo + 0.2*sigma_guessed)
-      #   n=ROOT.RooRealVar("nL","nL",2,0.1,15) 
-      #   nR=ROOT.RooRealVar("nR","nR",3,0.1,10)
-      #elif var_type in ['MC'] and regions in ['BE']:
-      #   mean=ROOT.RooRealVar("mean","mean",mean_guessed,mean_guessed -0.2*sigma_guessed, mean_guessed + 0.2*sigma_guessed)
-      #   n=ROOT.RooRealVar("nL","nL",3,0.1,15) 
-      #   nR=ROOT.RooRealVar("nR","nR",3,0.1,10)
-      #else :
-      #   mean=ROOT.RooRealVar("mean","mean",mean_histo,mean_histo -0.2*sigma_guessed, mean_histo + 0.2*sigma_guessed)
-      #   n=ROOT.RooRealVar("nL","nL",3,0.1,15) 
-      #   nR=ROOT.RooRealVar("nR","nR",4,0.1,10)
-
-      #Simple Fitting Functions
-      #fit_func=ROOT.RooCBShape("fit_func", "crystal ball", x,mean,sigma,alpha,n) 
-      #fit_func=ROOT.RooDCBShape("fit_func", "double crystal ball", x, mean, sigma, alpha, alphaR, n, nR)##va abbastanza bene
-      #fit_func=ROOT.RooGaussian("fit_func","gaussian",x, mean, sigma)
-      #fit_func=ROOT.RooBreitWigner("dfit_func", "breit wigner", x, mean, sigma)
-      #fit_func=ROOT.RooVoigtian("dfit_func", "voigtian (convolution gauss*breit wigner", x, mean,width, sigma)
 
       #Convolution Fit function (BW + dCB)
       bwMean=ROOT.RooRealVar("m_{Z}","BW Mean", 91.1876, "GeV") 
@@ -80,13 +40,16 @@ for var_type in ['data','MC']:
       bwWidth.setConstant(ROOT.kTRUE); 
       bw=ROOT.RooBreitWigner("bw", "breit wigner", x, bwMean, bwWidth)
 
-      #mean   = ROOT.RooRealVar("mean", "Double CB Bias", -.2, -20, 20, "GeV");
       mean      = ROOT.RooRealVar("mean", "Double CB Bias", -1., -4, 4);
       sigma     = ROOT.RooRealVar("sigma", "Double CB Width", 2., 0.5, 4.);
       dCBCutL   = ROOT.RooRealVar("al_{DCB}", "Double CB Cut left", 1., 0.1, 50.);
       dCBCutR   = ROOT.RooRealVar("ar_{DCB}", "Double CB Cut right", 1., 0.1, 50.);
       dCBPowerL = ROOT.RooRealVar("nl_{DCB}", "Double CB Power left", 2., 0.2, 50.);
       dCBPowerR = ROOT.RooRealVar("nr_{DCB}", "Double CB Power right", 2., 0.2, 50.);
+      #dCBCutL   = ROOT.RooRealVar("al_{DCB}", "Double CB Cut left", 1., 0.1, 1000.);
+      #dCBCutR   = ROOT.RooRealVar("ar_{DCB}", "Double CB Cut right", 1., 0.1, 1000.);
+      #dCBPowerL = ROOT.RooRealVar("nl_{DCB}", "Double CB Power left", 2., 0.2, 1000.);
+      #dCBPowerR = ROOT.RooRealVar("nr_{DCB}", "Double CB Power right", 2., 0.2, 1000.);
       if regions in ['BB'] and var_type in ['data']:
          mean      = ROOT.RooRealVar("mean", "Double CB Bias", 0.3, -4, 4);
          sigma     = ROOT.RooRealVar("sigma", "Double CB Width", 3, 0.5, 4.);
@@ -94,16 +57,15 @@ for var_type in ['data','MC']:
       dcb       = ROOT.RooDCBShape("dcb", "double crystal ball", x, mean, sigma, dCBCutL, dCBCutR, dCBPowerL, dCBPowerR)
 
       fit_func = ROOT.RooFFTConvPdf("fit_func","bw (X) dcb",x,bw,dcb)
-      
       fit_func.fitTo(dh)
 
-      res=fit_func.fitTo(dh,RooFit.Save()) #This is the general way of handling fit results
+      res=fit_func.fitTo(dh,RooFit.Save())
       mean_fit=res.floatParsFinal().find("mean").getVal()
       mean_fit_error=res.floatParsFinal().find("mean").getError()
       sigma_fit=res.floatParsFinal().find("sigma").getVal()
       sigma_fit_error=res.floatParsFinal().find("sigma").getError()
       #write fit parameters on file
-      file_res = open(str('../Extra_sigma/fit_extra_sigma_'+var_type+'_'+regions+'.dat'),'w+')
+      file_res = open(str('/user/gfasanel/Mass_resolution_study/Extra_sigma/fit_extra_sigma_'+var_type+'_'+regions+'.dat'),'w+')
       file_res.write("%lf %lf %lf %lf\n"%(mean_fit,mean_fit_error,sigma_fit/bwMean.getVal(),sigma_fit_error/bwMean.getVal()))
 
       #Plot and save the fit
@@ -114,41 +76,44 @@ for var_type in ['data','MC']:
       #I want to save the histogram and the fit in a file: how is it done? RooWorkSpace?? Add this later
       c = ROOT.TCanvas("fit","fit",800,800) #X length, Y length
       c.cd()
-      mean_label =ROOT.TLatex(0.12,0.85,"mean = %.4f #pm %.4f"%(mean_fit,mean_fit_error))
+      #c.SetLogy()
+      mean_label =ROOT.TLatex(0.14,0.85,"mean = %.4f #pm %.4f"%(mean_fit,mean_fit_error))
       mean_label.SetTextSize(0.04)
       mean_label.SetNDC()
       chi2=frame.chiSquare()
-      sigma_label =ROOT.TLatex(0.12,0.8,"sigma = %.4f #pm %.4f"%(sigma_fit,sigma_fit_error))
+      sigma_label =ROOT.TLatex(0.14,0.8,"sigma = %.4f #pm %.4f"%(sigma_fit,sigma_fit_error))
       sigma_label.SetNDC()
       sigma_label.SetTextSize(0.04)
       chi2=frame.chiSquare()
 
-      text= ROOT.TLatex(0.12,0.75,"#chi^{2}_{reduced} = %.2f" %chi2)
+      text= ROOT.TLatex(0.14,0.75,"#chi^{2}_{reduced} = %.2f" %chi2)
       text.SetNDC()
       text.SetTextSize(0.04)
-      entry= ROOT.TLatex(0.12,0.65,"Entries = %d" %hist_res.GetEntries())
+      entry= ROOT.TLatex(0.14,0.65,"Entries = %d" %hist_res.GetEntries())
       entry.SetNDC()
       entry.SetTextSize(0.04)
 
-      cms =ROOT.TLatex(0.12,0.95,"CMS Internal")
+      cms =ROOT.TLatex(0.6,0.96,"CMS Internal")
       cms.SetNDC()
       cms.SetTextSize(0.04)
       
       #frame.SetYTitle("test") # change y title
+      #frame.SetLabelOffset(0.03,"Y")
       frame.Draw() 
-      text.Draw()
+      #text.Draw() #chi2_reduced
       entry.Draw()
       mean_label.Draw()
       sigma_label.Draw()
       cms.Draw()
 
-      c.SaveAs(str('../Extra_sigma/'+hist_res.GetName()+'.png'))
-      c.SaveAs(str('../Extra_sigma/'+hist_res.GetName()+'.pdf'))
+      c.SaveAs(str('/user/gfasanel/Mass_resolution_study/Extra_sigma/'+hist_res.GetName()+'.png'))
+      c.SaveAs(str('/user/gfasanel/Mass_resolution_study/Extra_sigma/'+hist_res.GetName()+'.pdf'))
 
-print "[STATUS] Calling python ../Extra_sigma/sigma_extra.py"
-os.system("source ./sigma_extra_publisher.sh")              
-#os.system("python ../Extra_sigma/sigma_extra.py") # Ma perche' cazzo non funziona????
-#os.system("python /user/gfasanel/HEEP/CMSSW_7_2_0_patch1/src/Mass_resolution/Extra_sigma")
+print "[STATUS] Calling python /user/gfasanel/Mass_resolution_study/Extra_sigma/sigma_extra.py"
+os.system("cp /user/gfasanel/Mass_resolution_study/Extra_sigma/*.png ~/public_html/Res_scale_15/Extra_sigma/")
+os.system("cp /user/gfasanel/Mass_resolution_study/Extra_sigma/*.pdf ~/public_html/Res_scale_15/Extra_sigma/")
+#os.system("source ./roofit/sigma_extra_publisher.sh")#to publish on lxplus
+#os.system("python /user/gfasanel/Mass_resolution_study/Extra_sigma/sigma_extra.py") # Ma perche' cazzo non funziona????
 print "Now, write your .txt and latex table with:"
 print "python ../Extra_sigma/sigma_extra.py"
 
