41ac731734f38a84b3eff721f210f78337dd8e34
gfasanel <gfasanel@cern.ch>
2015-10-17 19:08:30 +0200

    circumvent M machines time limit

--- resolution_histos_maker.sh
+++ resolution_histos_maker.sh
@@ -21,5 +21,4 @@ python histos_for_resolution.py -i 18
 python histos_for_resolution.py -i 20
 python histos_for_resolution.py -i 21
 python histos_for_resolution.py -i 22
-#python histos_for_resolution.py -i 23
 hadd -f Resolution/histograms_mass_res.root  Resolution/histograms_mass_res_*.root
