bd319c39a5a296d15af6b17dcf47a61cf00989e5
gfasanel <gfasanel@cern.ch>
2015-10-25 19:12:57 +0100

    scp to lxplus

---
+++ AN_sender.sh
@@ -0,0 +1,3 @@
+dest=gfasanel@lxplus.cern.ch:/afs/cern.ch/user/g/gfasanel/CERN_documents/svnrepo/notes/AN-15-222/trunk/Strategy/images/
+scp Extra_sigma/*.pdf Extra_sigma/latex_extra_sigma.tex Resolution/fit_results/resolution_*pdf ${dest}
+#Resolution/fit_results/scale_BB/*.pdf ${dest}
