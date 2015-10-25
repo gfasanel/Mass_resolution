620b977fe50774b4378f484d44ca84c25a454a96
gfasanel <gfasanel@cern.ch>
2015-10-25 19:18:08 +0100

    update

--- AN_sender.sh
+++ AN_sender.sh
@@ -1,3 +1,5 @@
+source=/user/gfasanel/public_html/Res_scale_15
 dest=gfasanel@lxplus.cern.ch:/afs/cern.ch/user/g/gfasanel/CERN_documents/svnrepo/notes/AN-15-222/trunk/Strategy/images/
-scp Extra_sigma/*.pdf Extra_sigma/latex_extra_sigma.tex Resolution/fit_results/resolution_*pdf ${dest}
+
+scp ${source}/Extra_sigma/*.pdf Extra_sigma/latex_extra_sigma.tex ${source}/fit_results/resolution_*pdf ${source}/fit_results/scale_BB/scale_BB.pdf ${source}/fit_results/scale_BE/scale_BE.pdf ${source}/fit_results/scale_EE/scale_EE.pdf ${dest}
 #Resolution/fit_results/scale_BB/*.pdf ${dest}
