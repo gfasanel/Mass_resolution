6db270ce00c7c07c53f732d97b2dbcf3c11f1496
gfasanel <gfasanel@cern.ch>
2016-06-17 17:57:29 +0200

    AN sender update

--- AN_sender.sh
+++ AN_sender.sh
@@ -1,5 +1,4 @@
-source=/user/gfasanel/public_html/Res_scale_15
-dest=gfasanel@lxplus.cern.ch:/afs/cern.ch/user/g/gfasanel/CERN_documents/svnrepo/notes/AN-15-222/trunk/Strategy/images/
+source=/user/gfasanel/public_html/Res_scale_16
+dest=gfasanel@lxplus.cern.ch:/afs/cern.ch/user/g/gfasanel/CERN_documents/svnrepo/notes/AN-16-190/trunk/chapters/mass_resolution_figures
 
-scp ${source}/Extra_sigma/*.pdf Extra_sigma/latex_extra_sigma.tex ${source}/fit_results/resolution_*pdf ${source}/fit_results/scale_BB/scale_BB.pdf ${source}/fit_results/scale_BE/scale_BE.pdf ${source}/fit_results/scale_EE/scale_EE.pdf ${dest}
-#Resolution/fit_results/scale_BB/*.pdf ${dest}
+scp ${source}/Extra_sigma/*.pdf Extra_sigma/latex_extra_sigma_2016.tex ${source}/fit_results/resolution_*pdf ${source}/fit_results/scale_*.pdf ${source}/fit_results/HoverE/HoverE_*.pdf ${source}/fit_results/HoverE/HTotoverETot_*.pdf ${source}/fit_results/HoverE/eta_distributions.png ${dest}
